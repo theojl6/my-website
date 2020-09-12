22388172b7223d2d8bca2022ad672601257f2fe8
theojl6 <68571283+theojl6@users.noreply.github.com>
2020-09-11 17:09:57 -0700

    change static plugin

---
+++ wp-content/plugins/wp2static-master/tools/build_release.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+######################################
+##
+## Build WP2Static for wp.org
+##
+## script archive_name dont_minify
+##
+## places archive in $HOME/Downloads
+##
+######################################
+
+# run script from project root
+EXEC_DIR=$(pwd)
+
+TMP_DIR=$HOME/plugintmp
+rm -Rf $TMP_DIR
+mkdir -p $TMP_DIR
+
+rm -Rf $TMP_DIR/wp2static
+mkdir $TMP_DIR/wp2static
+
+
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps and optimize loader
+composer install --no-dev --optimize-autoloader
+
+
+# cp all required sources to build dir
+cp -r $EXEC_DIR/src $TMP_DIR/wp2static/
+cp -r $EXEC_DIR/vendor $TMP_DIR/wp2static/
+cp -r $EXEC_DIR/views $TMP_DIR/wp2static/
+cp -r $EXEC_DIR/*.php $TMP_DIR/wp2static/
+
+cd $TMP_DIR
+
+# tidy permissions
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
+
+zip -r -9 ./$1.zip ./wp2static
+
+cd -
+
+mkdir -p $HOME/Downloads/
+
+cp $TMP_DIR/$1.zip $HOME/Downloads/
+
+# reset dev dependencies
+cd $EXEC_DIR
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps
+composer install
---
+++ wp-content/plugins/wp2static-master/wp2static.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Plugin Name: WP2Static
+ * Plugin URI:  https://wp2static.com
+ * Description: Static site generator functionality for WordPress.
+ * Version:     7.1.0
+ * Author:      WP2Static
+ * Author URI:  https://wp2static.com
+ * Text Domain: wp2static
+ *
+ * @package     WP2Static
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+define( 'WP2STATIC_PATH', plugin_dir_path( __FILE__ ) );
+
+if ( file_exists( WP2STATIC_PATH . 'vendor/autoload.php' ) ) {
+    require_once WP2STATIC_PATH . 'vendor/autoload.php';
+}
+
+WP2Static\Controller::init( __FILE__ );
+
+function plugin_action_links( $links ) {
+    $settings_link =
+        '<a href="admin.php?page=wp2static">' .
+        __( 'Settings', 'static-html-output-plugin' ) .
+        '</a>';
+    array_unshift( $links, $settings_link );
+
+    return $links;
+}
+
+add_filter(
+    'plugin_action_links_' .
+    plugin_basename( __FILE__ ),
+    'plugin_action_links'
+);
+
+function wp2static_deregister_scripts() {
+    wp_deregister_script( 'wp-embed' );
+    wp_deregister_script( 'comment-reply' );
+}
+
+add_action( 'wp_footer', 'wp2static_deregister_scripts' );
+
+// TODO: move into own plugin for WP cleanup, don't belong in core
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+if ( defined( 'WP_CLI' ) ) {
+    WP_CLI::add_command( 'wp2static', 'WP2Static\CLI' );
+    WP_CLI::add_command( 'wp2static options', [ 'WP2Static\CLI', 'options' ] );
+}
+
